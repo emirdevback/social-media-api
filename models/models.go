e898638566e4ffaebc74824f59ba7ac3454bc0cd
emirdevback <onsekiznitro@gmail.com>
2026-05-18 20:37:48 +0300

    Update models.go

--- models/models.go
+++ models/models.go
@@ -7,9 +7,10 @@ type User struct {
 }
 
 type Post struct {
-	ID     int    `json:"id"`
-	UserId int    `json:"user_id"`
-	Body   string `json:"body"`
+	ID        int    `json:"id"`
+	UserId    int    `json:"user_id"`
+	Body      string `json:"body"`
+	LikeCount int    `json:"like_count"`
 }
 
 var Users []User
