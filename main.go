a7b01fa0016ae2081f0ebdc6d6c5eb8639a6f9a9
emirdevback <onsekiznitro@gmail.com>
2026-05-18 20:37:58 +0300

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ func main() {
 
 	http.HandleFunc("/users", handlers.UsersHandler)
 	http.HandleFunc("/posts", handlers.PostHandler)
+	http.HandleFunc("/post", handlers.GetPostHandler)
 	http.HandleFunc("/register", handlers.RegisterHandler)
+	http.HandleFunc("/create-post", handlers.CreatePostHandler)
+	http.HandleFunc("/like-post", handlers.LikePostHandler)
 
 	fmt.Println("Sosyal Medya Backend Motoru Paket Mantığıyla Açıldı: http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
